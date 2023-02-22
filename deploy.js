5879ca9f9d32577af2c8681a658a977bf5871f6c
Chatrola_Mit <chatrolamit047@gmail.com>
2023-02-22 20:28:47 +0530

    initial commit

--- deploy.js
+++ deploy.js
@@ -7,14 +7,14 @@ async function main(){
     // const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545")
     let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
 
-    // const wallet = new ethers.Wallet(
-    //     process.env.PRIVATE_KEY,
-    //     provider
-    // )
+    const wallet = new ethers.Wallet(
+        process.env.PRIVATE_KEY,
+        provider
+    )
 
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json" , "utf8")
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PASSWORD)
-    wallet = await wallet.connect(provider) 
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json" , "utf8")
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PASSWORD)
+    // wallet = await wallet.connect(provider) 
 
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi" , "utf8")
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin" , "utf8")
@@ -27,8 +27,8 @@ async function main(){
     const currentFavouriteNumber = await contract.retrive()
     console.log(currentFavouriteNumber.toString())
     
-    const txResponce = await contract.store("7");
-    const txRecipt = txResponce.wait(1);
+    const txResponce = await contract.store(7);
+    await txResponce.wait(1);
     
     const current = await contract.retrive()
     console.log(current.toString())
